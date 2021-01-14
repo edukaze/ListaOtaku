3b35ad2eb8c5fdbda41486992f5dd73647408dc6
edukaze <eduardo.bezerra.kaze@gmail.com>
2021-01-13 21:36:39 -0300

    adicionando o primeiro scripts e models da primeira tabela animes

---
+++ database/migrations/2021_01_13_232224_create_animes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnimesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 20);
+            $table->dateTimeTz('lancamento', $precision = 0);
+            $table->string('estudio')-> nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('animes');
+    }
+}
